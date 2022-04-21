5f0996770b19e167aaab2d1dbe33d6fab6d2f916
MatheusDonadoni2 <62766278+MatheusDonadoni2@users.noreply.github.com>
2022-04-21 18:55:54 -0300

    uo

--- Origin/main.js
+++ Origin/main.js
@@ -1,4 +1,4 @@
-// abre e fechar menu
+//Open and close menu
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -8,7 +8,7 @@ for (const element of toggle) {
   })
 }
 
-// Quando clicar em um item do menu, escoder o menu
+// Quando clicar em um item do menu, esconder o menu
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
@@ -18,10 +18,11 @@ for (const link of links) {
 }
 
 //Mudar o Header da página quando der Scroll
-const header = document.querySelector('#header');
-const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header');
+  const navHeight = header.offsetHeight;
+
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header  
     header.classList.add('scroll')
@@ -29,7 +30,7 @@ window.addEventListener('scroll', function () {
     // scroll é menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
 // Testimonials Slider, Swiper, Carrosel
 const swiper = new Swiper('.swiper', {
@@ -54,5 +55,22 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `, { interval: 100 })
+
+// Botão voltar para o topo
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top');
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+//When Scroll
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
