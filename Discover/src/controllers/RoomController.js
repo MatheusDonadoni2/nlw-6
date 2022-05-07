59316e2915e4ee1f0176e93b2d32aa999a1e0578
MatheusDonadoni2 <62766278+MatheusDonadoni2@users.noreply.github.com>
2022-05-07 20:34:14 -0300

    up

--- Discover/src/controllers/RoomController.js
+++ Discover/src/controllers/RoomController.js
@@ -1,8 +1,37 @@
+const Database = require("../db/config")
 module.exports = {
-  create(req, res) {
-    let roomId = 123456
+  async create(req, res) {
+    const db = await Database()
+    const pass = req.body.password
+    let roomId
+    let isRoom = true
 
-    createInfoBD = {}
+    while (isRoom) {
+      for (var i = 0; i < 6; i++) {
+        if (i == 0) {
+          roomId = Math.floor(Math.random() * 10).toString()
+        }
+        else {
+          roomId += Math.floor(Math.random() * 10).toString()
+        }
+      }
+
+      const roomsExistIds = await db.all(`SELECT id from rooms`)
+      isRoom = roomsExistIds.some(roomsExistIds => roomsExistIds === roomId)
+      if (!isRoom) {
+        await db.run(
+          `INSERT INTO rooms(
+            id, 
+            pass) 
+            VALUES(
+            ${parseInt(roomId)}, 
+            ${pass})`)
+      }
+    }
+
+
+
+    await db.close
 
     res.redirect(`room/${roomId}`)
   }
--- Discover/src/db/init.js
+++ Discover/src/db/init.js
@@ -2,7 +2,21 @@ const DataBase = require("./config")
 
 const initDb = {
   async init() {
-    const db = await Database()
-    await db.exec
+    const db = await DataBase()
+    await db.exec(
+      `CREATE TABLE rooms(
+    id INTEGER PRIMARY KEY,
+    pass TEXT)`)
+
+    await db.exec(
+      `CREATE TABLE question(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      titulo TEXT,
+      read INT)`)
+
+    await db.close
   }
 }
+
+initDb.init();
+
